5cc2c6a0899d18ff53731a46440261e508530987
David Azofeifa <36681942+DavidAzofeifa@users.noreply.github.com>
2022-09-13 21:05:55 -0700

    Update 2. sp_Rebuild_vDynamicsData.sql

--- 2. sp_Rebuild_vDynamicsData.sql
+++ 2. sp_Rebuild_vDynamicsData.sql
@@ -1,101 +1,102 @@
-CREATE OR ALTER PROCEDURE [dbo].[sp_Rebuild_vDynamicsData] AS
+CREATE OR ALTER   PROCEDURE [dbo].[sp_Rebuild_vDynamicsData] AS
 
 BEGIN
 
+    DECLARE @viewprefix NVARCHAR(MAX) = 'vDynamicsData'; -- You can change the prefix of the views here
 	DECLARE @sql NVARCHAR(MAX) = '';
+	
+    WITH
 
-	WITH
+        enabledEntities (entityName) AS
+        (
+            SELECT entityName FROM
+            (
+                VALUES
+                    ('account') /* Add more lines to include other entities */
+                    --,('dp_country')
+                    --,('dp_manufacturer') 
+            ) e(entityName)
+        ),
 
-		enabledEntities (entityName) AS
-		(
-			SELECT entityName FROM
-			(
-				VALUES
-					('account') /* Add more lines to include other entities */
-					--,('dp_country')
-					--,('dp_manufacturer') 
-			) e(entityName)
-		),
+        columnDefinitions AS
+        (
+            SELECT
 
-		columnDefinitions AS
-		(
-			SELECT
+                entityName,
 
-				entityName,
+                columns4select = 
+                    STRING_AGG
+                    (
+                        CONCAT
+                        (
+                            CONVERT(NVARCHAR(MAX), QUOTENAME(columnName)),
+                            CASE
+                                WHEN dataTypeTemp = dataTypeFinal THEN ''
+                                ELSE CONCAT(' = CONVERT(', dataTypeFinal, ', ', CONVERT(NVARCHAR(MAX), QUOTENAME(columnName)), ')')
+                            END                    
+                        ),
+                        CONCAT(',', CHAR(13), CHAR(10), SPACE(4*7))
+                    )
+                    WITHIN GROUP (ORDER BY columnID),  
 
-				columns4select = 
-					STRING_AGG
-					(
-						CONCAT
-						(
-							CONVERT(NVARCHAR(MAX), QUOTENAME(columnName)),
-							CASE
-								WHEN dataTypeTemp = dataTypeFinal THEN ''
-								ELSE CONCAT(' = CONVERT(', dataTypeFinal, ', ', CONVERT(NVARCHAR(MAX), QUOTENAME(columnName)), ')')
-							END                    
-						),
-						CONCAT(',', CHAR(13), CHAR(10), SPACE(4*7))
-					)
-					WITHIN GROUP (ORDER BY columnID),  
-
-				columns4with = 
-					STRING_AGG
-					(
-						CONCAT
-						(
-							CONVERT(NVARCHAR(MAX), QUOTENAME(columnName)),
-							' ',
-							dataTypeTemp, 
-							' ', 
-							columnID
-						),
-						CONCAT(',', CHAR(13), CHAR(10), SPACE(4*8))
-					)
-					WITHIN GROUP (ORDER BY columnID)
+                columns4with = 
+                    STRING_AGG
+                    (
+                        CONCAT
+                        (
+                            CONVERT(NVARCHAR(MAX), QUOTENAME(columnName)),
+                            ' ',
+                            dataTypeTemp, 
+                            ' ', 
+                            columnID
+                        ),
+                        CONCAT(',', CHAR(13), CHAR(10), SPACE(4*8))
+                    )
+                    WITHIN GROUP (ORDER BY columnID)
             
-				FROM
-					vDynamicsModel
-				GROUP BY
-					entityName
-		)
+                FROM
+                    vDynamicsModel
+                GROUP BY
+                    entityName
+        )
         
-		SELECT
-			@sql = 
-				STRING_AGG
-				(
-					CONCAT
-					(
-						'
-						EXEC(''
+        SELECT
+            @sql = 
+                STRING_AGG
+                (
+                    CONCAT
+                    (
+                        '
+                        EXEC(''
 
-						CREATE OR ALTER VIEW [vDynamicsData_', UPPER(cd.entityName), '] AS
+                        CREATE OR ALTER VIEW [', @viewprefix, '_', UPPER(cd.entityName), '] AS
 
-						SELECT
-							', cd.columns4select, '
-						FROM
-							OPENROWSET
-							(
-								BULK ''''/', cd.entityName, '/*.csv'''',
+                        SELECT
+                            ', cd.columns4select, '
+                        FROM
+                            OPENROWSET
+                            (
+                                BULK ''''/', cd.entityName, '/*.csv'''',
 								DATA_SOURCE = ''''DataSource'''',
-								FORMAT = ''''CSV'''',
-								PARSER_VERSION = ''''2.0''''
-							)
-							WITH
-							(
-								', cd.columns4with,'
-							) AS [result]
+                                FORMAT = ''''CSV'''',
+                                PARSER_VERSION = ''''2.0''''
+                            )
+                            WITH
+                            (
+                                ', cd.columns4with,'
+                            ) AS [result]
                 
-						'')
-						'
-					),
-					'; ' /* separator */
-				) WITHIN GROUP (ORDER BY cd.entityName)
-		FROM
-			columnDefinitions cd
-			INNER JOIN enabledEntities e ON e.entityName = cd.entityName
+                        '')
+                        '
+                    ),
+                    '; ' /* separator */
+                ) WITHIN GROUP (ORDER BY cd.entityName)
+        FROM
+            columnDefinitions cd
+            INNER JOIN enabledEntities e ON e.entityName = cd.entityName
 
-	PRINT @sql
-	EXEC (@sql)
+    PRINT @sql
+    EXEC (@sql)
 
 END
 
